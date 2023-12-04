1c27bb985f6be75fe2fee92e82a974d9107cdfb8
sabr <a663469@gmail.com>
2023-12-04 10:38:13 +0300

    Improved the output and saving of logs

--- sshGetDump.sh
+++ sshGetDump.sh
@@ -7,4 +7,8 @@ ssh pi@192.168.71.159 "sudo i2cdump -y 1 0x09 w" > $tmp
 cat $tmp
 echo $tmp
 
-python2 ./start.py $tmp
+python2 ./start.py $tmp > description_$tmp
+cat description_$tmp
+
+mkdir ./dumps/$(date +%j-%H-%M-%S)
+mv ./*.txt ./dumps/$(date +%j-%H-%M-%S)/
--- sshSetReg.sh
+++ sshSetReg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
 ssh pi@192.168.71.159 "sudo i2cset -r -y 1 0x09 $1 $2 w"
-ssh pi@192.168.71.159 "sudo i2cget -y 1 0x09 $1 w"
+# ssh pi@192.168.71.159 "sudo i2cget -y 1 0x09 $1 w"
 
 
